cf9f82159f81a35d07882e65b51c4538784171ba
SadratMetalfire <sebastianderegowski@onet.pl>
2025-06-03 22:06:27 +0200

    Fix mistake

--- page-poslowie.php
+++ page-poslowie.php
@@ -1,6 +1,4 @@
-<?php get_header();
+<?php
 
 // Load archive-posel.php to avoide duplicates (might change when spliting into template parts)
 include locate_template('archive-posel.php');
-
-get_footer();
